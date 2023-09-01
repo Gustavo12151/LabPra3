0ab559e15675e93655d1c449f92353489d1165cd
Gustavo12151 <gustavorosales2199@gmail.com>
2023-08-31 23:29:29 -0300

    Initial commit

---
+++ src/main/java/com/mycompany/labpra3/LabPra3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.labpra3;
+
+/**
+ *
+ * @author guty
+ */
+public class LabPra3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
